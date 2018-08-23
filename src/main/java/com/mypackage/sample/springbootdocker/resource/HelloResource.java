716b0c7f4297f5af479b997f03e8c884b7c5bc90
ulag-india <ulag.india@gmail.com>
2018-08-24 00:29:45 +0530

    Base Version

---
+++ src/main/java/com/mypackage/sample/springbootdocker/resource/HelloResource.java
@@ -0,0 +1,15 @@
+package com.mypackage.sample.springbootdocker.resource;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest/docker/hello")
+public class HelloResource {
+
+	@GetMapping
+	public String getMessage() {
+		return "Hello World!";
+	}
+}
